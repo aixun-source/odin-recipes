df8f6a29da24c4d8627cc0f282d685f0b9c34de6
aixun-source <132420500+aixun-source@users.noreply.github.com>
2024-09-19 13:06:49 +0800

    Added a branch rps-ui and changed the game
    Now you can click on the individual buttons.
    Honestly the biggest learning point is to always
    defer the js when you are referencing elements
    within the HTML, because if you don't, then the js
    is taking stuff that has not been created yet. and
    will obviously not do anything.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -13,15 +13,16 @@ function getComputerChoice(){
     }
 }
 
+const nameBut = document.querySelector("#name");
 function askNameFunction(){
     let name = prompt("What's your name?");
     let answerElement = document.getElementById("answer");
     answerElement.innerText = "Hello, " + name;
 }
+nameBut.addEventListener("click", askNameFunction);
 
-function playRound(){
+function playRound(humanChoice){
     let score = 0;
-    let humanChoice = prompt("What's your choice?");
     let computerChoice = getComputerChoice();
 
     if (humanChoice == "rock" && computerChoice == "scissors"){
@@ -38,21 +39,56 @@ function playRound(){
         score--; //human lost
     } 
 
-    return score;
-}
+    return {human: humanChoice, comp: computerChoice, score: score};
+} //this function now returns an array
 
-function playGame(){
-    let rounds = prompt("How many rounds?");
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < rounds; i++) {
-        let roundResult = playRound();
-        if (roundResult == 1){
-            humanScore++;
-        } else if (roundResult == -1){
-            computerScore++;
-        }
+const centerDiv = document.querySelector('#rps-center');
+const resultPara = document.querySelector('#result');
+
+centerDiv.addEventListener('click', (event) => {
+    let target = event.target;
+    //let result;
+
+    //play the game
+    switch(target.id){
+        case "rock":
+            result = playRound("rock");
+            break;
+        case "paper":
+            result = playRound("paper");
+            break;
+        case "scissors":
+            result = playRound("scissors");
+            break;
     }
-    let resultElement = document.getElementById("result");
-    resultElement.innerText = "You have gotten " + humanScore + " points!";
-}
+    
+    //print results
+    if (result.score == -1) {
+        console.log("You Lost!");
+        resultPara.textContent = `You lost! ${result.human} is beaten by ${result.comp}!`;
+    } else if (result.score == 1) {
+        console.log("You Won!");
+        resultPara.textContent = `You won! ${result.human} beats ${result.comp}!`;
+    } else if (result.score == 0){
+        console.log("A tie!");
+        resultPara.textContent = `A tie! ${result.human} ties ${result.comp}!`;
+    }
+});
+
+//from previous exercise
+// function playGame(){
+//     let rounds = prompt("How many rounds?");
+//     let humanScore = 0;
+//     let computerScore = 0;
+//     for (let i = 0; i < rounds; i++) {
+//         let roundResult = playRound();
+//         if (roundResult == 1){
+//             humanScore++;
+//         } else if (roundResult == -1){
+//             computerScore++;
+//         }
+//     }
+//     let resultElement = document.getElementById("result");
+//     resultElement.innerText = "You have gotten " + humanScore + " points!";
+// }
+
