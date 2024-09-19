75a4f5760f396d678f26ef839a4db57787bc5610
aixun-source <132420500+aixun-source@users.noreply.github.com>
2024-09-19 18:45:01 +0800

    Pause for today
    Have removed Init button event, because need to
    figure out how to make grid fit only the box
    without scrolling.
    
    Currently still experimenting, but I swear it has
    something to do with flex.

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,36 +1,23 @@
 const initButton = document.querySelector('.but');
 const grid = document.querySelector('.grid'); //the original div
+let input = 2;
 
-function initGrid(){
-    let input = prompt("How big? Between 2 to 10 only.");
-    let divInGrid = document.createElement("div");
-    divInGrid.style.cssText = "flex-grow: 1";
-    divInGrid.classList.add("divgrid"); //creates another div IN grid.
-    //to store rows
+let divInGrid = document.createElement("div");
+divInGrid.style.cssText = "display: flex; flex-direction: column; flex-grow: 1; width: 100%";
+divInGrid.classList.add("divgrid"); //creates another div IN grid.
+//to store rows
 
-    //init rows
-    for (let i = 0; i < input; i++){
-        let divRow = document.createElement("div"); //the row div
-        divRow.classList.add("row");
-        divRow.style.cssText = "display: flex";
-        for (let i = 0; i < input; i++) {
-            let divBox = document.createElement("div"); //the box div
-            divBox.classList.add("box");
-            divBox.style.cssText = "border: 1px solid red; flex: 1 1 auto; aspect-ratio: 1/1"; //for testing
-            divRow.appendChild(divBox);
-        }
-        divInGrid.appendChild(divRow);
+//init rows
+for (let i = 0; i < input; i++){
+    let divRow = document.createElement("div"); //the row div
+    divRow.classList.add("row");
+    divRow.style.cssText = "display: flex; flex: 1 1 auto";
+    for (let i = 0; i < input; i++) {
+        let divBox = document.createElement("div"); //the box div
+        divBox.classList.add("box");
+        divBox.style.cssText = "border: 1px solid red; flex: 1 1 0; aspect-ratio: 1/1"; //for testing
+        divRow.appendChild(divBox);
     }
-
-    let divInGridCheck = document.querySelector('.divgrid'); //checks if div IN grid exists
-    if (grid.contains(divInGridCheck)){
-        grid.removeChild(divInGridCheck);
-        grid.appendChild(divInGrid);
-    } else {
-        grid.appendChild(divInGrid);
-    }
-    //if there is grid there, delete and replace.
-    //if there is no grid there, add it in
+    divInGrid.appendChild(divRow);
 }
-
-initButton.addEventListener('click', initGrid);
+grid.appendChild(divInGrid);
