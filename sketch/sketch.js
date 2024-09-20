b98ad46351e6c0b5326dc29aacab665c30a4d9c1
aixun-source <132420500+aixun-source@users.noreply.github.com>
2024-09-20 10:53:49 +0800

    Added in the javascript.
    Works, still don't think we are in the sketch-test
    branch but that's okay. Also I didn't change
    any of the code, but I think it just needed a
    refresh then it started working again...?
    Not sure how that went.

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,23 +1,37 @@
-// const initButton = document.querySelector('.but');
+const initButton = document.querySelector('.but');
 const grid = document.querySelector('.grid'); //the original div
-let input = 4;
 
-let divInGrid = document.createElement("div");
-divInGrid.style.cssText = "display: flex; flex-direction: column";
-divInGrid.classList.add("divInGrid"); //creates another div IN grid.
-//to store rows
+function initGrid(){
+    let input = prompt("How big? Between 2 to 10 only.");
+    let divInGrid = document.createElement("div");
+    divInGrid.style.cssText = "display: flex; flex-direction: column";
+    divInGrid.classList.add("divInGrid"); //creates another div IN grid.
+    //to store rows
 
-//init rows
-for (let i = 0; i < input; i++){
-    let divRow = document.createElement("div"); //the row div
-    divRow.classList.add("row");
-    divRow.style.cssText = `display: flex; height: calc(100vh/${input}); width: 100vh; border: 1px solid blue`;
-    for (let i = 0; i < input; i++) {
-        let divBox = document.createElement("div"); //the box div
-        divBox.classList.add("box");
-        divBox.style.cssText = `aspect-ratio: 1/1; width: calc(100vh/${input}); border: 1px solid red`; //for testing
-        divRow.appendChild(divBox);
+    //init rows
+    for (let i = 0; i < input; i++){
+        let divRow = document.createElement("div"); //the row div
+        divRow.classList.add("row");
+        divRow.style.cssText = `display: flex; height: calc(100vh/${input}); width: 100vh; border: 1px solid blue`;
+        for (let i = 0; i < input; i++) {
+            let divBox = document.createElement("div"); //the box div
+            divBox.classList.add("box");
+            divBox.style.cssText = `aspect-ratio: 1/1; width: calc(100vh/${input}); border: 1px solid red`; //for testing
+            divRow.appendChild(divBox);
+        }
+        divInGrid.appendChild(divRow);
     }
-    divInGrid.appendChild(divRow);
+
+    //to determine if divInGrid already exists:
+    let divInGridCheck = document.querySelector('.divInGrid'); //checks if div IN grid exists
+    if (grid.contains(divInGridCheck)){
+        grid.removeChild(divInGridCheck);
+        grid.appendChild(divInGrid);
+    } else {
+        grid.appendChild(divInGrid);
+    }
+    //if there is grid there, delete and replace.
+    //if there is no grid there, add it in
 }
-grid.appendChild(divInGrid);
+
+initButton.addEventListener('click', initGrid);
