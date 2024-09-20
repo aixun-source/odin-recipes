1690d352d533e9643b9f038b9f916d3a96bdd440
aixun-source <132420500+aixun-source@users.noreply.github.com>
2024-09-20 10:38:39 +0800

    Figured out the css, now need to add it into js
    Somehow the branch does not work?? am currently
    checking but i seems like the branch registers in
    vsc but not in git bash? not sure what is going
    on but committing just in case work is lost.

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,21 +1,21 @@
-const initButton = document.querySelector('.but');
+// const initButton = document.querySelector('.but');
 const grid = document.querySelector('.grid'); //the original div
-let input = 2;
+let input = 4;
 
 let divInGrid = document.createElement("div");
-divInGrid.style.cssText = "display: flex; flex-direction: column; flex-grow: 1; width: 100%";
-divInGrid.classList.add("divgrid"); //creates another div IN grid.
+divInGrid.style.cssText = "display: flex; flex-direction: column";
+divInGrid.classList.add("divInGrid"); //creates another div IN grid.
 //to store rows
 
 //init rows
 for (let i = 0; i < input; i++){
     let divRow = document.createElement("div"); //the row div
     divRow.classList.add("row");
-    divRow.style.cssText = "display: flex; flex: 1 1 auto";
+    divRow.style.cssText = `display: flex; height: calc(100vh/${input}); width: 100vh; border: 1px solid blue`;
     for (let i = 0; i < input; i++) {
         let divBox = document.createElement("div"); //the box div
         divBox.classList.add("box");
-        divBox.style.cssText = "border: 1px solid red; flex: 1 1 0; aspect-ratio: 1/1"; //for testing
+        divBox.style.cssText = `aspect-ratio: 1/1; width: calc(100vh/${input}); border: 1px solid red`; //for testing
         divRow.appendChild(divBox);
     }
     divInGrid.appendChild(divRow);
